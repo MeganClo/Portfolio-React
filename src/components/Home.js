c8722b515cf07d6370e025517314cca3d7b21ce8
Megs <Megs.close@gmail.com>
2021-06-08 12:40:40 -0700

    testing some styling-don't like it-needs work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,13 @@
+import image from "../background.jpg"
+
 const Home = () => {
     return (
-        <h1>Home!</h1>
+        <main>
+            <img src={image} alt="plant leaves background" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6-xl bold font-bold text-blue-100 leading-non lg:leading-snug home-name">Hello. I'm Megan.</h1>
+            </section>
+        </main>
     )
 }
 
