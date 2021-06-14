bd1cc53f2c47ac445b86718cddac7c681dda4cd4
Megs <Megs.close@gmail.com>
2021-06-13 17:34:52 -0700

    errors still flying

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,6 @@
 import { useState, useEffect } from "react";
 import sanityClient from "../client";
 import nature from "../nature.jpeg";
-import imageUrlBuilder from "@sanity/image-url";
-
-const builder = imageUrlBuilder(sanityClient);
-function urlFor(source) {
-    return builder.image(source);
-}
 
 const About = () => {
     const [author, setAuthor] = useState(null); 
@@ -15,10 +9,16 @@ const About = () => {
         sanityClient.fetch(`*[_type == "author"]{
             name,
             bio,
-            "authorImage": image.asset->url
+            image{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            },
         }`)
-        // .then((data) => console.log(data))
-        .then((data) => setAuthor(data[0]))
+        .then((data) => console.log(data[0]))
+        // .then((data) => setAuthor(data[0]))
 
 
 
@@ -30,11 +30,11 @@ const About = () => {
             <img src={nature} alt="abstract nature" className="absolute object-cover w-full"/>
             <div className="p-10 lg:pt-48 containter mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} alt="author" className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/>
+                    {/* <img src={image} alt="author" className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/> */}
                     <div>
                         <h1>
                             <div>
-                                BLOCK Content
+                                {/* {author.name} */}
                             </div>
                         </h1>
                     </div>
