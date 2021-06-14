2160f047b99bf0c564db9db0a14bae67c7e92568
Megs <Megs.close@gmail.com>
2021-06-13 17:13:38 -0700

    errors flying when trying to use sanity url-will try without it

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import sanityClient from "../client";
 import nature from "../nature.jpeg";
-import imageUrlBuilder from "./@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -9,22 +9,28 @@ function urlFor(source) {
 }
 
 const About = () => {
-    const [about, setAbout] = useState(null); 
+    const [author, setAuthor] = useState(null); 
 
     useEffect(() => {
-        sanityClient.fetch(`*[type == "author"]{
+        sanityClient.fetch(`*[_type == "author"]{
             name,
             bio,
             "authorImage": image.asset->url
-        }`).then((data) => setAbout(data[0]))
+        }`)
+        // .then((data) => console.log(data))
+        .then((data) => setAuthor(data[0]))
+
+
+
         .catch(console.error);
     }, []);
+
     return (
         <main className="relative">
             <img src={nature} alt="abstract nature" className="absolute object-cover w-full"/>
             <div className="p-10 lg:pt-48 containter mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} alt=""/>
+                    <img src={urlFor(author.authorImage).url()} alt="author" className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/>
                     <div>
                         <h1>
                             <div>
