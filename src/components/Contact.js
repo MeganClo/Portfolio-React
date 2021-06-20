0080fcb5de7666541ab8814fe852bae2ca6e15f2
Megs <Megs.close@gmail.com>
2021-06-19 20:37:15 -0700

    validation not working as wanted

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,51 @@
 import { useState } from "react";
+import { validateEmail } from "../utils/helpers"
 
 const Contact = () => {
     const [formState, setFormState] = useState({ firstName: "", lastName: "", email: "", message: "" });
+    const [errorMessage, setErrorMessage] = useState('');
     const { firstName, lastName, email, message } = formState;
+
     function handleChange(e) {
-        setFormState({ ...formState, [e.target.name]: e.target.value })
-    };
+        if (e.target.name === "email") {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if (!isValid) {
+                setErrorMessage("Please provide a valid email");
+                // console.log(errorMessage)
+            } else {
+                setErrorMessage("");
+            }
+        } else {
+            console.log(!e.target.value.length)
+            // console.log("hello")
+            if (!e.target.value.length) {
+                // console.log(e.target.value)
+                setErrorMessage("Please fill out all fields.");
+            } else {
+                setErrorMessage("");
+            }
+        } 
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+        
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
+        console.log("error message", errorMessage)
     }
+
     return (
         <main className="bg-black min-h-screen p-12">
 
             <div className="container mx-auto my-20 w-2/3 border border-red-800 bg-gray-200">
                 <div className="p-5 space-y-5 shadow-x1">
                     <h4 className="light text-center text-3xl">Contact Me</h4>
-                    <form onSubmit={handleSubmit} >
+                    <form onSubmit={handleSubmit}>
                         <div className="grid grid-cols-2 gap-5">
                             <input placeholder="First Name" className="border border-red-500 px-4 py-2 focus:outline-none focus:border-red-800" type="text" name="firstName" defaultValue={firstName} onChange={handleChange} />
                             <input placeholder="Last Name" className="border border-red-500 px-4 py-2 focus:outline-none focus:border-red-800" type="text" name="lastName" defaultValue={lastName} onChange={handleChange} />
@@ -28,7 +57,14 @@ const Contact = () => {
                         <div className="grid grid-cols-1 pt-5">
                             <textarea name="message" placeholder="Write your message..." className="border border-red-500 px-4 py-2 focus:outline-none focus:border-red-800" cols="" rows="5" defaultValue={message} onChange={handleChange}></textarea>
                         </div>
-                        <input className="light focus:outline-none mt-5 bg-red-800 px-4 py-2 text-white font-bold w-full" type="submit"/>
+                        <input className="light focus:outline-none mt-5 bg-red-800 px-4 py-2 text-white font-bold w-full" type="submit" />
+                        <div>
+                        {errorMessage && (
+							<div>
+								<p className="bold font-bold text-center">{errorMessage}</p>
+							</div>
+						)}
+                        </div>
                     </form>
                 </div>
             </div>
